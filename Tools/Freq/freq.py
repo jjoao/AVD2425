e6d9ca019e0b8b2ae79173df2f04836353303470
jjoao <jj@di.uminho.pt>
2025-05-09 08:50:19 +0100

    generalized print:
    -r print lema, freqabs freqpmill usual rank racio

--- Tools/Freq/freq.py
+++ Tools/Freq/freq.py
@@ -22,27 +22,40 @@ from collections import Counter
 pln = spacy.load("pt_core_news_lg")
 voc = pln.vocab
 freqsum = Counter()
-freq = Counter()
+freqabs = Counter()
 freqrel = Counter()
 
-def printtab(f,m=1000000):
-    for pal,oco in f.most_common(m):
-        freqestim = freqsum[pal]/freq[pal]*1000000
-        racio = oco / freqestim
+def gprinttab(fabs,frel,m=1000000):
+    print(f"word\toccu\tfreq-per-mill\tusual\trank\tracio")
+    for pal,oco in fabs.most_common(m):
+        
+        freqhabitual = freqsum[pal]/freqabs[pal]*1000000
+        racio = frel[pal] / freqhabitual
         if (r:= voc[pal].rank) > 1000000 :
             r = "OFV"
-        print(f"{pal}\t{oco:.2f}\t{freqsum[pal]/freq[pal]*1000000:.2f}\t{r}\t{racio:.2f}")
+        print(f"{pal}\t{oco}\t{frel[pal]:.2f}\t{freqhabitual:.2f}\t{r}\t{racio:.2f}")
+
+def printtab(f,m=1000000):
+    print(f"word\toccu\tfreq-per-mill\trank\tracio")
+    for pal,oco in f.most_common(m):
+        freqhabitual = freqsum[pal]/freqabs[pal]*1000000
+        racio = oco / freqhabitual
+        if (r:= voc[pal].rank) > 1000000 :
+            r = "OFV"
+        print(f"{pal}\t{oco:.2f}\t{freqsum[pal]/freqabs[pal]*1000000:.2f}\t{r}\t{racio:.2f}")
 
 def rank2freq(w,voc):
-    if (r:= voc[w].rank) > 1000000 :
+    r= min( voc[w].rank , voc[w.lower()].rank)
+    if r > 1000000 :
         r = 1000000
     return 1 / (r + 2.7)  ## Zipf law variant
 
 def main():
-    cl = clfilter("wlrm:", doc=__doc__ ) ### cl.opt
+    cl = clfilter("pwlrm:", doc=__doc__ ) ### cl.opt
     
     # -m 10   how many values 
     num = int(cl.opt.get("-m",1000000))
+    
     # -r      rel or abs
     isrel = False
     if "-r" in cl.opt: 
@@ -56,16 +69,18 @@ def main():
                 continue 
             if tok.pos_ != "PROPN":
                 lem = re.sub(r" .*", r"", lem)
-            freq[lem] +=1 
+            else:
+                lem = tok.text
+            freqabs[lem] +=1 
             freqsum[lem] += rank2freq(tok.text, voc) 
 
-    tot = freq.total()
+    tot = freqabs.total()
+    for pal,oco in freqabs.items():
+        freqrel[pal] = oco / tot * 1000000
     if isrel:   ## relative freq
-        for pal,oco in freq.items():
-            freqrel[pal] = oco / tot * 1000000
-        printtab(freqrel, num)
+        gprinttab(freqabs, freqrel,num)
     else:
-        printtab(freq, num )
+        printtab(freqabs, num )
 
 if __name__ == "__main__" : 
     main()
